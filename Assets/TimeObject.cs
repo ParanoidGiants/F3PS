c0534cf4ee3232d8a51f73db05facc8bef4e5eec
locojoetive <johannes.mic@gmail.com>
2023-09-27 13:21:38 +0200

    feat: use mass for slowdown

--- Assets/TimeObject.cs
+++ Assets/TimeObject.cs
@@ -1,67 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeObject : MonoBehaviour
 {
-    public float customTimeScale = 1.0f;
+    private const double TOLERANCE = 0.001f;
+    private Rigidbody _rb;
+    private float _defaultMass;
 
-    private Rigidbody rb;
-    private Vector3 lastVelocity;
-    public bool isPitched = false;
-    public bool pitch = false;
-    
+    public float currentTimeScale;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+        _defaultMass = _rb.mass;
     }
 
-    double TOLERANCE = 0.001f;
     void FixedUpdate()
     {
-        pitch = Math.Abs(customTimeScale - 1.0f) > TOLERANCE;
-        
-        
-        if (pitch && !isPitched)
-        {
-            isPitched = true;
-            lastVelocity = rb.velocity;
-            rb.velocity = lastVelocity * customTimeScale;
-            rb.useGravity = false;
-        }
-
-        if (isPitched)
-        {
-            
-            if (fCollided)
-            { // if collision happened...
-                Debug.Log("ADJUST");
-                fCollided = false; // reset flag
-                // calculate acceleration due to collision
-                var acc = (rb.velocity - fLastVel) * customTimeScale;
-                // convert to force:
-                // var force = rb.mass * acc;
-                // call OnAfterCollision passing the Collision 
-                // info and the reaction force:
-                rb.velocity = Vector3.zero;
-                rb.AddForce(acc, ForceMode.VelocityChange);
-                rb.angularVelocity *= customTimeScale * 2f;
-            }
-
-            var newGravity = customTimeScale * Physics.gravity;
-            rb.AddForce(newGravity, ForceMode.Acceleration);
-            
-            fLastVel = rb.velocity; // update last velocity
-        }
+        var scaledGravity = currentTimeScale * Physics.gravity;
+        _rb.AddForce(scaledGravity, ForceMode.Acceleration);
     }
-    
-    private bool fCollided = false;
-    private Vector3 fLastVel;
-    private Collision fCollision;
 
-    private void OnCollisionEnter(Collision coll){
-        fCollision = coll; // save collision data
-        fCollided = true; // signal that a collision happened
+    void PitchTimeScale(float newTimeScale)
+    {
+        var unscaledVelocity = _rb.velocity / currentTimeScale;
+        _rb.mass = _defaultMass / (newTimeScale*newTimeScale);
+        _rb.velocity = unscaledVelocity * newTimeScale;
+        currentTimeScale = newTimeScale;
     }
 }
