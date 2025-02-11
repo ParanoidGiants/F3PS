fdedd491f51469766dcb336645f07d2699f7d745
locojoetive <johannes.mic@gmail.com>
2025-02-11 10:05:04 +0100

    animate time grenade

--- Assets/_Game/Entities/Weapon/TimeGrenade/TimeBubbleGrenadeProjectile.cs
+++ Assets/_Game/Entities/Weapon/TimeGrenade/TimeBubbleGrenadeProjectile.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using StarterAssets;
 using UnityEngine;
 
@@ -6,19 +7,48 @@ public class TimeBubbleGrenadeProjectile : BaseProjectile
     [Header("References")]
     public TimeBubble timeBubble;
     public new Collider collider;
+    public float animationDuration = 0.5f;
+    public float targetScale = 20f;
+    private bool _isActive = false;
 
     public float Gravity => _timeObject.gravityScale * Physics.gravity.magnitude;
     public float LifeTimePercentage => lifeTime / maximumLifeTime;
 
     private void Update()
     {
-        if (!_isHit) return;
-        
+        if (!_isHit || !_isActive) return;
+
         lifeTime += Time.deltaTime;
         if (lifeTime > maximumLifeTime)
         {
-            gameObject.SetActive(false);
+            DeactivateTimeBubble();
         }
+    }
+
+    private void ActivateTimeBubble()
+    {
+        timeBubble.gameObject.SetActive(true);
+        timeBubble.transform.localScale = Vector3.zero;
+        timeBubble.transform
+            .DOScale(Vector3.one * targetScale, animationDuration)
+            .SetEase(Ease.OutCubic)
+            .OnComplete(() =>
+             {
+                 _isActive = true;
+             });
+    }
+
+    private void DeactivateTimeBubble()
+    {
+        _isActive = false;
+        timeBubble.gameObject.transform.DOScale(Vector3.zero, animationDuration)
+            .SetEase(Ease.InCubic)
+            .OnComplete(() =>
+            {
+                // Deactivate the GameObject after the tween is complete.
+                timeBubble.gameObject.SetActive(false);
+                gameObject.SetActive(false);
+            });
     }
 
     override
@@ -50,7 +80,7 @@ public class TimeBubbleGrenadeProjectile : BaseProjectile
         _rb.constraints = RigidbodyConstraints.FreezeAll;
         collider.enabled = false;
         GetComponent<TimeObject>().enabled = false;
-        timeBubble.gameObject.SetActive(true);
+        ActivateTimeBubble();
     }
 
 }
