64458fd9dce9bed436b1a9f9f55c481b32bdccd8
locojoetive <johannes.mic@gmail.com>
2025-02-14 11:58:22 +0100

    move shake to hittable manager

--- Assets/_Game/System/Hittable/HittableManager.cs
+++ Assets/_Game/System/Hittable/HittableManager.cs
@@ -1,10 +1,19 @@
+using Cinemachine;
 using UnityEngine;
 
 public class HittableManager : MonoBehaviour
 {
     public Collider[] colliders;
-    void Awake()
+    public CinemachineImpulseSource shakeSource;
+    public float shakePower;
+
+    private void Awake()
     {
         colliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void Shake(float damageMultiplier)
+    {
+        shakeSource.GenerateImpulse(damageMultiplier * shakePower);
     }
 }
