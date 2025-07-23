b2ce7992027cfdf21fa870cd416923e0a873ed52
locojoetive <johannes.mic@gmail.com>
2025-07-23 18:39:03 +0200

    draft revert-ball

---
+++ Assets/RevertBall.cs
@@ -0,0 +1,50 @@
+using System;
+using StarterAssets;
+using UnityEngine;
+
+public class RevertBall : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Transform _targetPlacePoint;
+    public Transform _spawnPoint;
+    public float _lifeTime;
+    public float _lifeDuration;
+
+    public void Init(Transform targetPlacePoint, Transform spawnPoint, float lifeTime)
+    {
+        _targetPlacePoint = targetPlacePoint;
+        _spawnPoint = spawnPoint;
+        _lifeDuration = lifeTime;
+    }
+
+    private void Update()
+    {
+        _lifeTime += Time.deltaTime;
+        if (_lifeTime >= _lifeDuration)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.TryGetComponent<ThirdPersonController>(out var player))
+        {
+
+            return;
+        }
+        player.transform.position = _targetPlacePoint.position;
+        gameObject.SetActive(false);
+
+    }
+
+    private void Onable()
+    {
+        transform.position = _spawnPoint.position;
+        transform.rotation = Quaternion.identity;
+        _rb = GetComponent<Rigidbody>();
+        _rb.linearVelocity = Vector3.down;
+        _rb.angularVelocity = Vector3.zero;
+        _lifeTime = 0f;
+    }
+}
---
+++ Assets/RevertBallSpawner.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RevertBallSpawner : MonoBehaviour
+{
+    public GameObject revertBallPrefab;
+    public List<RevertBall> revertBalls;
+    public Transform targetPlacePoint;
+    public Transform ballSpawnPoint;
+    public int ballCount;
+    public float time;
+    public float spawnEverySeconds;
+    public float ballLifeDuration;
+    public int currentlySpawnedBallIndex = 0;
+
+    private void Awake()
+    {
+        for (int i = 0; i < ballCount; i++)
+        {
+            var spawnPosition = ballSpawnPoint.position;
+            var ball = Instantiate(revertBallPrefab, spawnPosition, ballSpawnPoint.rotation);
+            var revertBall = ball.GetComponent<RevertBall>();
+            revertBall.Init(targetPlacePoint, ballSpawnPoint, ballLifeDuration);
+            revertBalls.Add(revertBall);
+            ball.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time < spawnEverySeconds)
+        {
+            return;
+        }
+        time %= spawnEverySeconds;
+
+        var nextBall = revertBalls[currentlySpawnedBallIndex];
+        nextBall.gameObject.SetActive(true);
+        currentlySpawnedBallIndex++;
+        if (currentlySpawnedBallIndex >= ballCount)
+        {
+            currentlySpawnedBallIndex = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.TryGetComponent<RevertBall>(out var ball))
+        {
+            return;
+        }
+        ball.gameObject.SetActive(false);
+    }
+}
