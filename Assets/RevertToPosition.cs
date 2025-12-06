02cce72f6985d7b6a7e074bd14390244cfdef8f7
locojoetive <johannes.mic@gmail.com>
2025-12-06 10:16:54 +0100

    replace collision by trigger; otherwise when player falls too slow she won't collide with this collider

--- Assets/RevertToPosition.cs
+++ Assets/RevertToPosition.cs
@@ -5,12 +5,11 @@ public class RevertToPosition : MonoBehaviour
 {
     public Transform _revertPlayerToPoint;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collider)
     {
-        if (collision.gameObject.TryGetComponent<ThirdPersonController>(out var player))
+        if (collider.gameObject.TryGetComponent<ThirdPersonController>(out var player))
         {
             player.FreezeAndRevertToPosition(_revertPlayerToPoint.position, _revertPlayerToPoint.rotation);
-            return;
         }
     }
 }
