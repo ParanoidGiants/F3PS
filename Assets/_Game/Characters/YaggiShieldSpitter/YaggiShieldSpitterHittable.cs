e9cdab5ef16947b79aec913e1a79ad0c3ca2e6b2
locojoetive <johannes.mic@gmail.com>
2025-12-10 08:38:02 +0100

    buff spitter yaggi

--- Assets/_Game/Characters/YaggiShieldSpitter/YaggiShieldSpitterHittable.cs
+++ Assets/_Game/Characters/YaggiShieldSpitter/YaggiShieldSpitterHittable.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class YaggiShieldSpitterHittable : Hittable
 {
     public YaggiShieldSpitterController yaggi;
--- Assets/_Game/Characters/YaggiStandard/YaggiStandardHittable.cs
+++ Assets/_Game/Characters/YaggiStandard/YaggiStandardHittable.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class YaggiStandardHittable : Hittable
 {
     public YaggiStandardController yaggi;
